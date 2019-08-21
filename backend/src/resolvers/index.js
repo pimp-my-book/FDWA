cbc49d80795381819ae5a3b5b95c07f86c86b82c
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-08-21 15:33:44 +0200

    UPDATE => GraphQL Index

--- backend/src/resolvers/index.js
+++ backend/src/resolvers/index.js
@@ -3,9 +3,9 @@ import { addMenuItem } from "./mutation";
 
 export const resolvers = {
     Query: {
-        hello: (root, args, context) => hello(args, context)
+        hello: (args, context) => hello(args, context)
     },
     Mutation: {
-        addMenuItem: (root, args, context) => addMenuItem(args, context)
+        addMenuItem: (args, context) => addMenuItem(args, context)
     }
 }
