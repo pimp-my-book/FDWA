6050d421336c3e9531a2928e92ea4f2e49ba5134
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-08-21 11:59:09 +0200

    IN-PROGESS => addMenuItem Mutation

--- backend/src/resolvers/mutation.js
+++ backend/src/resolvers/mutation.js
@@ -3,9 +3,16 @@
 ADD YOUR MUTATION HERE:
 
 export const addCars = async (args, context) => {
-    
+
 
 }
 
 
 */
+
+export const addMenuItem = async (args, context) => {
+  name: String!,
+  price: Int!,
+  vendor: String!,
+  size: String!
+}
