849fbc75a2571bc350f562218f64d4ed686f0590
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-08-21 15:35:07 +0200

    ADD => Mutations

--- backend/src/resolvers/mutation.js
+++ backend/src/resolvers/mutation.js
@@ -9,7 +9,20 @@ export const addCars = async (args, context) => {
 
 
 */
+import uuid from "uuid";
 
 export const addMenuItem = async (args, context) => {
-  
+  ID: uuid.v1();
+  name: args.name;
+  price: args.price;
+  vendor: args.vendor;
+  size: args.size
+}
+
+export const placeOrder = async (args, context) => {
+  ID: uuid.v1();
+  total: args.total;
+  items: args.items;
+  deliveryMethod: args.deliveryMethod;
+  deliveryCost: args.deliveryCost;
 }
