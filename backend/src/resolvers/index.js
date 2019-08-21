8986f4c82b646ed0cf163ff1ea4de185b4afa87a
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-08-21 11:58:30 +0200

    UPDATE => Add Mutation to Index

--- backend/src/resolvers/index.js
+++ backend/src/resolvers/index.js
@@ -1,7 +1,11 @@
-import {hello} from "./query";
+import { hello } from "./query";
+import { addMenuItem } from "./mutation";
 
 export const resolvers = {
     Query: {
         hello: (root, args, context) => hello(args, context)
+    },
+    Mutation: {
+        addMenuItem: (root, args, context) => addMenuItem(args, context)
     }
 }
