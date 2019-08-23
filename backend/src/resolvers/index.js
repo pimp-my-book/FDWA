8e4d5fc234394ef2901ed084942125eac77018eb
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-08-23 11:47:44 +0200

    UUPDATE => Import getAllItems

--- backend/src/resolvers/index.js
+++ backend/src/resolvers/index.js
@@ -1,9 +1,10 @@
-import { hello } from "./query";
+import { hello, getAllItems } from "./query";
 import { addMenuItem } from "./mutation";
 
 export const resolvers = {
     Query: {
-        hello: (args, context) => hello(args, context)
+        hello: (args, context) => hello(args, context),
+        getAllItems: (args, context) => getAllItems(args, context)
     },
     Mutation: {
         addMenuItem: (root, {input:args}, context) => addMenuItem({input:args}, context)
