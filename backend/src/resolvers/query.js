cf88c4b329f08902576913343192de92abd9f501
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-08-23 10:08:20 +0200

    ADD => getAllItems [Query]

--- backend/src/resolvers/query.js
+++ backend/src/resolvers/query.js
@@ -1,5 +1,28 @@
-import * as dynamoDBLib from "../../libs/dynamodb-lib";
+import * as dynamodbLib from "../../libs/dynamodb-lib";
 
 export const hello = (args, context) => {
     return "First FDWA Query -_-"
+}
+
+export const getAllItems = async (args, context) => {
+  const params ={
+    TableName: process.env.FoodDeliveryTable,
+    KeyConditionExpression: "itemName = :itemName",
+    ExpressionAttributeValues: {
+      ":itemName": args.itemName
+    }
+  }
+  try {
+    const result = await dynamodbLib.call("query", params);
+    if (result.Item) {
+      return result.Item;
+    }
+    else {
+      return "No items found within table"
+    }
+  }
+  catch (e) {
+    console.log(e);
+    return e;
+  }
 }
