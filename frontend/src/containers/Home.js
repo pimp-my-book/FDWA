342945237c67269c3bb431ae63b5253207244ac8
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-08-27 10:09:27 +0200

    UPDATE => Home [Queries + Mutations]

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Button, ButtonToolbar, Grid, Col, Row } from 'react-bootstrap';
 import "../styles/styles.css";
 
 export default class Home extends Component {
@@ -8,6 +9,20 @@ export default class Home extends Component {
         <div className="landingPage">
           <h1>Pimp My Book</h1>
           <h3>Food Delivery Web Application [GraphQL - Test Suite]</h3>
+          <Grid>
+            <Row>
+              <Col xs={6} md={4}>
+              </Col>
+              <Col xs={6} md={4}>
+                <ButtonToolbar classname=".homeButtonToolbar">
+                  <Button bsStyle="primary" bsStyle="primary">Queries</Button>
+                  <Button bsStyle="primary" bsStyle="primary">Mutations</Button>
+                </ButtonToolbar>
+              </Col>
+              <Col xs={6} md={4}>
+              </Col>
+            </Row>
+          </Grid>
         </div>
       </div>
     );
