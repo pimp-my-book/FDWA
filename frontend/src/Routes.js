c743f8e3a22123c9acaeac1e2f29fae1c8f626ba
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-08-27 14:05:24 +0200

    UPDATE => Add Route [Hello World - Query]

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import helloWorld from "./containers/helloWorld";
 
 export default () =>
   <Switch>
-    <Route path="/" exact component={Home} />
+    <Route exact path="/" component={ Home } />
+    <Route exact path="/queries/helloWorld" component={ helloWorld } />
   </Switch>;
