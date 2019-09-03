cf453a0a998ded34e1e7b9c23c1275cccf11903d
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-03 10:30:03 +0200

    UPDATE => helloWorld [Container + Query]

--- frontend/src/containers/helloWorld.js
+++ frontend/src/containers/helloWorld.js
@@ -1,8 +1,7 @@
-import React, { Component, Fragment } from "react";
-import { Button, ButtonToolbar, Grid, Col, Row } from "react-bootstrap";
+import React, { Component } from "react";
+import { Grid, Col, Row } from "react-bootstrap";
 import { Query } from "react-apollo";
 import { helloWorld } from "../graphql/Queries";
-import alertComponent from "../components/alertComponent";
 import "../styles/styles.css";
 
 export default class helloWorldQuery extends Component {
@@ -12,52 +11,37 @@ export default class helloWorldQuery extends Component {
         <div className="landingPage">
           <h1>Hello World</h1>
           <h3>GraphQL - Hello World Query</h3>
-          <Grid>
-            <Row>
-              <Col xs={6} md={4}>
-              </Col>
-              <Col xs={6} md={4}>
-                <h5><u>Output</u></h5>
-              </Col>
-              <Col xs={6} md={4}>
-              </Col>
-            </Row>
-            <Row>
-              <Col xs={6} md={4}>
-              </Col>
-              <Col xs={6} md={4}>
-                <Query query={ helloWorld }>
-                  {
-                    ({ data, loading, error }) => {
-                      // On load [Display Heading]
-                      if (loading) return (
-                        <Grid>
-                          <Row>
-                            <Col>
-                              <h6>loading...</h6>
-                            </Col>
-                          </Row>
-                        </Grid>
-                      );
-
-                      // On query fail [Print Error]
-                      if (error) return (
-                        <Fragment>
-                          <alertComponent text = { error.message.replace("GraphQL Error:", "") } variant="danger" />
-                        </Fragment>
-                      );
-
-                      //
-
-                    }
-                  }
-                </Query>
-                />
-              </Col>
-              <Col xs={6} md={4}>
-              </Col>
-            </Row>
-          </Grid>
+            <Grid>
+              <Row>
+                <Col xs={6} md={4}>
+                </Col>
+                <Col xs={6} md={4}>
+                  <h5><u>Output</u></h5>
+                </Col>
+                <Col xs={6} md={4}>
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={6} md={4}>
+                </Col>
+                <Col xs={6} md={4}>
+                  <Query query={ helloWorld }>
+                    {({error, loading, data}) => {
+                      if (loading) return <div>Fetching</div>;
+                      if (error) return <div>Error</div>;
+                      const returnValue = data.hello
+                      return(
+                        <div>
+                          {returnValue}
+                        </div>
+                      )
+                    }}
+                  </Query>
+                </Col>
+                <Col xs={6} md={4}>
+                </Col>
+              </Row>
+            </Grid>
         </div>
       </div>
     );
