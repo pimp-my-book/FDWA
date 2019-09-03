36bed18fb137d2bb1da8af859489d36071bb100d
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-03 14:51:03 +0200

    ADD => getAllItems Query

--- frontend/src/graphql/Queries.js
+++ frontend/src/graphql/Queries.js
@@ -5,3 +5,16 @@ export const HELLO_WORLD = gql `
  query helloWorld {
    hello
  }`;
+
+// GraphQL - getAllItems
+export const GET_ALL_ITEMS = gql `
+query GetAllItems{
+  getAllItems{
+    pk
+    sk
+    itemName
+    price
+    vendorName
+    size
+  }
+}`;
