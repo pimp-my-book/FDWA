f50e4c6a8916fc57b66ec4aa317ea517f11b9f41
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-03 16:45:35 +0200

    ADD => customerOrderList [Backend + index.js]

--- backend/src/resolvers/index.js
+++ backend/src/resolvers/index.js
@@ -1,10 +1,11 @@
-import { hello, getAllItems } from "./query";
+import { hello, getAllItems, customerOrderList } from "./query";
 import { addMenuItem, placeOrder } from "./mutation";
 
 export const resolvers = {
     Query: {
         hello: (args, context) => hello(args, context),
-        getAllItems: (args, context) => getAllItems(args, context)
+        getAllItems: (args, context) => getAllItems(args, context),
+        customerOrderList: (args, context) => customerOrderList(args, context)
     },
     Mutation: {
         addMenuItem: (root, {input:args}, context) => addMenuItem({input:args}, context),
