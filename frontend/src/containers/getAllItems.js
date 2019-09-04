dab9cb4c576db11114e233e088235faf42d45acd
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-04 15:05:26 +0200

    getAllItems Query Output Container

---
+++ frontend/src/containers/getAllItems.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { Query } from "react-apollo";
+import { GET_ALL_ITEMS } from "../graphql/Queries";
+import { Grid, Col, Row } from "react-bootstrap";
+import "../styles/styles.css";
+
+export default class GetAllItems extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="landingPage">
+          <h1>Get All Items</h1>
+          <h3>GraphQL - Get All Items Query</h3>
+            <Grid>
+              <Row>
+                <Col xs={6} md={4}>
+                </Col>
+                <Col xs={6} md={4}>
+                  <h5><u>Output</u></h5>
+                </Col>
+                <Col xs={6} md={4}>
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={6} md={4}>
+                </Col>
+                <Col xs={6} md={4}>
+                  <Query query={ GET_ALL_ITEMS }>
+                    {({ data, loading, error }) => {
+                      if (loading) return <div>Fetching</div>;
+                      if (error) return <div>Error</div>;
+                      const allItems = data.getAllItems;
+                      console.log(allItems);
+                      }
+                    }
+                  </Query>
+                </Col>
+                <Col xs={6} md={4}>
+                </Col>
+              </Row>
+            </Grid>
+        </div>
+      </div>
+    );
+  }
+}
