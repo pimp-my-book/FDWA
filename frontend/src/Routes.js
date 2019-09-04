30ac3788b60425724222184ddd421df87db89000
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-04 15:06:15 +0200

    getAllItems Container Routing

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,9 +2,11 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import helloWorld from "./containers/helloWorld";
+import GetAllItems from "./containers/getAllItems";
 
 export default () =>
   <Switch>
     <Route exact path="/" component={ Home } />
     <Route exact path="/queries/helloWorld" component={ helloWorld } />
+    <Route path="/queries/getAllItems" exact component={ GetAllItems } />
   </Switch>;
