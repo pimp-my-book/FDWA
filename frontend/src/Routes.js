bb38b9b8cdc74107486b6b567e0ca1139bff61a6
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-09 15:57:27 +0200

    Add PlaceOrder container to Routes

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,11 +3,13 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import helloWorld from "./containers/helloWorld";
 import GetAllItems from "./containers/GetAllItems";
+import PlaceOrder from "./containers/PlaceOrder";
 
 export default () =>
   <Switch>
-    <Route exact path="/" component={ Home } />
-    <Route exact path="/queries/helloWorld" component={ helloWorld } />
+    <Route path="/" exact component={ Home } />
+    <Route path="/queries/helloWorld" exact component={ helloWorld } />
     <Route path="/queries/getAllItems" exact component={ GetAllItems } />
+    <Route path="/mutations/placeOrder" exact component={ PlaceOrder } />
   </Switch>;
   
