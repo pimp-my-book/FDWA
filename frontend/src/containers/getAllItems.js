98c0d194703309d1490ba43e73d12826638f0d76
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-09 14:28:57 +0200

    getAllItems => Fixed display grid & query return

--- frontend/src/containers/getAllItems.js
+++ frontend/src/containers/getAllItems.js
@@ -1,24 +1,18 @@
 import React, { Component, Fragment } from "react";
 import { Query } from "react-apollo";
 import { GET_ALL_ITEMS } from "../graphql/Queries";
-import {  Col, Row } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 import ItemCard from "../components/ItemCard";
 import styled from "styled-components";
 import "../styles/styles.css";
 
 export default class GetAllItems extends Component {
   render() {
-    const itemName = styled.div `
-      display: grid;
-      grid-gap: 20px;
-      grid-template-columns: repeat( 2, 3fr );
-    `;
     return (
       <div className="Home">
         <div className="landingPage">
           <h1>Get All Items</h1>
           <h3>GraphQL - Get All Items Query</h3>
-           
               <Row>
                 <Col xs={6} md={4}>
                 </Col>
@@ -28,39 +22,40 @@ export default class GetAllItems extends Component {
                 <Col xs={6} md={4}>
                 </Col>
               </Row>
-              <Row>
-                <Col xs={6} md={4}>
-                </Col>
-                <Col xs={6} md={4}>
-                  <Query query={ GET_ALL_ITEMS }>
-                    {({ data, loading, error }) => {
-                      if (loading) return <div>Fetching</div>;
-                      if (error) return <div>{Error}</div>;
-                      console.log(error)
-                      const allItems = data.getAllItems;
-                      console.log(allItems);
-                      return(
+              </div>
+              <div>
+                <Query query={ GET_ALL_ITEMS }>
+                  {({ data, loading, error }) => {
+                    if (loading) return <div>Fetching</div>;
+                    if (error) return <div>{Error}</div>;
+                    console.log(error)
+                    const allItems = data.getAllItems;
+                    console.log(allItems);
+                    const ItemsGrid = styled.div `
+                      display: grid;
+                      grid-template-columns: repeat( 3, 3fr );
+                      grid-gap: 10px;
+                      margin: 0%;
+                      padding-left: 10%;
+                    `;
+                    return(
+                      <ItemsGrid>
                         <Fragment>
                           {allItems.map((items) => (
                             <Fragment  key={ items.pk }>
-                             <ItemCard
-                              vendorName={ items.vendorName } 
-                              itemName={ items.itemName }
-                              itemPrice={ items.price }
-                              mealSize={ items.size } />
-                             </Fragment>
+                              <ItemCard
+                                vendorName={ items.vendorName } 
+                                itemName={ items.itemName }
+                                itemPrice={ items.price }
+                                mealSize={ items.size } />
+                            </Fragment>
                           ))}
-                          
                         </Fragment>
-                      );
-                      }
-                    }
-                  </Query>
-                </Col>
-                <Col xs={6} md={4}>
-                </Col>
-              </Row>
-            
+                      </ItemsGrid>
+                    );
+                  }
+                }
+                </Query>
         </div>
       </div>
     );
