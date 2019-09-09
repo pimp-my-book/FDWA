632b1bb8be299248b1e15f64842f4c8e6d39a687
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-09 12:34:43 +0200

    Export issue is fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,12 +9,12 @@ class App extends Component {
     return(
       <div className="appContainer">
         <Navbar fluid collapseOnSelect>
-          <Navbar.Header>
+          
             <Navbar.Brand>
               <Link to="/"><span role="img" aria-label="Pizza">🍕</span></Link>
             </Navbar.Brand>
             <Navbar.Toggle />
-          </Navbar.Header>
+          
         </Navbar>
         <Routes />
       </div>
--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import helloWorld from "./containers/helloWorld";
-import GetAllItems from "./containers/getAllItems";
+import GetAllItems from "./containers/GetAllItems";
 
 export default () =>
   <Switch>
@@ -10,3 +10,4 @@ export default () =>
     <Route exact path="/queries/helloWorld" component={ helloWorld } />
     <Route path="/queries/getAllItems" exact component={ GetAllItems } />
   </Switch>;
+  
