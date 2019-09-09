463cd3267adc71ea34db6a85ffe40c3c4c959956
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-09 14:30:19 +0200

    index.js => Fix httpLink to GraphQL API

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,9 +11,9 @@ import * as serviceWorker from './serviceWorker';
 
 const stage = process.env.REACT_APP_STAGE === "prod";
 
-//stage ? process.env.REACT_APP_GRAPHQL_ENDPNT_PROD : process.env.REACT_APP_GRAPHQL_ENDPNT_DEV
+// stage ? process.env.REACT_APP_GRAPHQL_ENDPNT_PROD : process.env.REACT_APP_GRAPHQL_ENDPNT_DEV
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000/graphql"
+  uri: stage ? process.env.REACT_APP_GRAPHQL_ENDPNT_PROD : process.env.REACT_APP_GRAPHQL_ENDPNT_DEV
 });
 
 const client = new ApolloClient({
