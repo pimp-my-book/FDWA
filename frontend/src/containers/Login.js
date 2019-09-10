f9f3a2073fc2680a26373652738a95ff80d07f6b
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-10 17:26:04 +0200

    Login Container

--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -1,4 +1,54 @@
-import React, { Component } from "react";
-import { Button, Form } from "react-bootstrap";
+import React, { Component, useState } from "react";
+import Auth from "aws-amplify";
+import { Button, Form, Container, Col } from "react-bootstrap";
 import "../styles/styles.css";
 
+const Login = ({  }) => {
+
+    const handleSubmit = async event => {
+        event.preventDefault();
+        try {
+            await Auth.signIn(email, password)
+        }
+        catch (e) {
+            console.log(e)
+        }
+    }
+
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+
+    return(
+        <div>
+            <Container className="auth-form-container">
+                <Form 
+                    onSubmit = { handleSubmit }
+                >
+                    <Form.Row>
+                        <Form.Group>
+                            <Form.Label as={ Col } controlId="formAuthEmail">Email</Form.Label>
+                                <Form.Control type="text" placeholder="Email address here..."
+                                    value = { email }
+                                    onChange = { e => setEmail( e.target.value ) }
+                                />
+                        </Form.Group>
+                    </Form.Row>
+                    <Form.Row>
+                        <Form.Group>
+                            <Form.Label as={ Col } controlId="formAuthPassword">Password</Form.Label>
+                                <Form.Control type="text" placeholder="Enter password here..."
+                                    value = { password }
+                                    onChange = { e => setPassword( e.target.value ) }
+                                />
+                        </Form.Group>
+                    </Form.Row>
+                    <Button variant="primary" type="submit">
+                        Login
+                    </Button>
+                </Form>
+            </Container>
+        </div>
+    );
+}
+
+export default Login
