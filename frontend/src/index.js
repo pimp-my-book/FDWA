48b12f1536876844f48e47e07a5923b1179e9257
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-10 12:16:22 +0200

    Set httpLink to Dev Endpoint

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,9 +11,8 @@ import * as serviceWorker from './serviceWorker';
 
 const stage = process.env.REACT_APP_STAGE === "prod";
 
-//stage ? process.env.REACT_APP_GRAPHQL_ENDPNT_PROD : process.env.REACT_APP_GRAPHQL_ENDPNT_DEV
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000/graphql"
+  uri: stage ? process.env.REACT_APP_GRAPHQL_ENDPNT_PROD : process.env.REACT_APP_GRAPHQL_ENDPNT_DEV
 });
 
 const client = new ApolloClient({
