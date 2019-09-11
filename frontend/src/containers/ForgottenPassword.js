5a44a57d596f08c6318fbebf683430abb6fcf782
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-11 17:30:31 +0200

    Add constructor

--- frontend/src/containers/ForgottenPassword.js
+++ frontend/src/containers/ForgottenPassword.js
@@ -3,3 +3,19 @@ import { Auth } from "react";
 import { Form, Container, Col, Row } from "react-bootstrap";
 import { validate } from "@babel/types";
 
+    constructor(props){
+		super(props);
+
+        this.state = {
+            email: "",
+            code: "",
+            password: "",
+            codeSent: false,
+            confirmed: false, 
+            confirmPassword: "",
+            isConfirming: false,
+            isSendingCode: false,
+            emailError: "",
+            confirmError: ""
+        };
+    }
