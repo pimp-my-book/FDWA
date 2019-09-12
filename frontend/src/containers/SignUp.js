e1b24466e3cca500b3df4a97fad0f569b53e06c0
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-12 17:27:14 +0200

    Add handleSubmit

--- frontend/src/containers/SignUp.js
+++ frontend/src/containers/SignUp.js
@@ -2,3 +2,15 @@ import React, { Components, useState } from "react";
 import Auth from "@aws-amplify/auth";
 import { Button, Form, Container, Col, Row } from "react-bootstrap";
 
+    const handleSubmit = async event => {
+        event.preventDefault();
+
+        try {
+            const newUser = await Auth.signUp(username, password);
+            console.log(newUser);
+            setNewUser(newUser)
+        }
+        catch (e) {
+            alert(e.message)
+        }
+    }
