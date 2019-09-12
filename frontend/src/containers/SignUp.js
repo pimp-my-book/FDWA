8ddf3203bd0fd6e5ca2c9eb5626d3f8a8b7fc2df
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-12 17:28:01 +0200

    Add Sign Up form

--- frontend/src/containers/SignUp.js
+++ frontend/src/containers/SignUp.js
@@ -33,3 +33,48 @@ import { Button, Form, Container, Col, Row } from "react-bootstrap";
     }
 
     const renderForm = () => {
+        return(
+          <Container>
+              <Row>
+                  <Col>
+                    <div>
+                        Sign Up
+                    </div>
+                  </Col>
+              </Row>
+              <Row>
+                  <Form onSubmit={ this.handleSubmit }>
+                    <Form.Row>
+                        <Form.Group controlId="sign-up-email">
+                            <Form.Label>Email Address</Form.Label>
+                                <Form.Control
+                                    type="email"
+                                    placeholder="Please enter your email address here..."
+                                    value={ this.username }
+                                    onChange = { e => setUsername( e.target.value ) }
+                                />
+                        </Form.Group>
+                    </Form.Row>
+                    <Form.Row>
+                        <Form.Group controlId="sign-up-password">
+                            <Form.Label>Password</Form.Label>
+                                <Form.Control
+                                    type="password"
+                                    placeholder="Please enter your password here..."
+                                    value={ this.password }
+                                    onChange = { e => setPassword( e.target.value ) }
+                                />
+                        </Form.Group>
+                    </Form.Row>
+                    <Form.Row>
+                        <Form.Group controlId="sign-up-password">
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+                        </Form.Group>
+                    </Form.Row>
+                  </Form>
+              </Row>
+          </Container>  
+        );
+    }
