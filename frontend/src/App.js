fcacc49ff335c85e8c51948d9057d2c15104afd6
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-13 13:57:10 +0200

    Fix imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { Navbar, Button } from "react-bootstrap";
+import { Auth } from "aws-amplify";
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
 import Routes from "./Routes";
 import './styles/styles.css';
-import { Auth } from "aws-amplify";
 
 class App extends Component {
   constructor( props ) {
