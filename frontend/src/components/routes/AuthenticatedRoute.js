a9ccfce6acff7ceb12fa34f5ba20aef70b95d7a3
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-13 17:26:23 +0200

    Add querystring function

--- frontend/src/components/routes/AuthenticatedRoute.js
+++ frontend/src/components/routes/AuthenticatedRoute.js
@@ -1,7 +1,22 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 
-export default ({ component: C, props: cProps, ...rest }) =>
+function querystring(name, url = window.location.href) {
+    name = name.replace(/[[]]/g, "\\$&");
+
+    const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)", "i");
+    const results = regex.exec(url);
+
+    if (!results) {
+        return null;
+    }
+    if (!results[2]) {
+        return "";
+    }
+
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 
 <Route
     { ...rest }
