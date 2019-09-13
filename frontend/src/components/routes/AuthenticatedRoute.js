34589e4f97305ddfe6c5fe8faa67b466d32fed0d
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-13 15:57:42 +0200

    Complete -> AuthRoute

--- frontend/src/components/routes/AuthenticatedRoute.js
+++ frontend/src/components/routes/AuthenticatedRoute.js
@@ -3,3 +3,13 @@ import { Route, Redirect } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import { mapContextToProps } from "react-context-toolbox";
 
+export default ({ component: Container, props: mapContextToProps, ...rest }) =>
+
+<Route
+    { ...rest }
+    render={ props =>
+        !mapContextToProps.isAuthenticated
+        ? <C { ...props } { ...cProps } />
+        : <Redirect to="/" />
+    }
+/>;
