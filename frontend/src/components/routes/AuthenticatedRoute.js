7ef5f44698f1ecbe15ceef0ba9560a54694e8932
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-13 15:58:21 +0200

    Remove unneeded

--- frontend/src/components/routes/AuthenticatedRoute.js
+++ frontend/src/components/routes/AuthenticatedRoute.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-import { Container } from "react-bootstrap";
-import { mapContextToProps } from "react-context-toolbox";
 
 export default ({ component: Container, props: mapContextToProps, ...rest }) =>
 
