3e6fd74827935dc09c57fbaac4510db8829856b4
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-13 15:57:27 +0200

    Add imports -> AuthRoute

---
+++ frontend/src/components/routes/AuthenticatedRoute.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { mapContextToProps } from "react-context-toolbox";
+
